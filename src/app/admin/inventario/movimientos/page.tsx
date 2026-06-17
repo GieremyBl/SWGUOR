ff4d56843a6560ab607978c80abd2633f6c3bd33
GieremyBL <daniel_9402@outlook.com>
2026-06-16 20:57:26 -0500

    .

--- src/app/admin/inventario/movimientos/page.tsx
+++
@@ -1,5 +0,0 @@
-import { MovimientosInventarioPage } from '@/components/admin/movimientos/MovimientosInventarioPage';
-
-export default function InventarioMovimientosPage() {
-  return <MovimientosInventarioPage />;
-}
